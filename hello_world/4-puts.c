cd831602b5f2b5235c91f5e49f63856acd072623
amarhalabi74-sys  <amarhalabi74@gmail.com>
2025-10-21 10:59:39 +0200

    Puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Point d'entrée du programme
+ *
+ * Description: Affiche une phrase avec puts
+ *
+ * Return: 0 si succès
+ */
+ int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+} 
