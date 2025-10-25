afdee2f75ec03345eebeaf6971f1dafd7b22e3d2
amarhalabi74-sys  <amarhalabi74@gmail.com>
2025-10-25 13:45:07 +0200

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int xx;
+for (xx = 0; xx < 10; xx++)
+{
+putchar('0' + xx);
+}
+putchar('\n');
+return (0);
+}
