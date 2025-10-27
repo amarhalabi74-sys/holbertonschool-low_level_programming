56a1ab7c5fbd884678a102c1d5c6967d121d52c2
amarhalabi74-sys  <amarhalabi74@gmail.com>
2025-10-27 15:52:39 +0100

    valeur absolue

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _abs - calculer la valeur absolue d'un entier
+* @r: l'entier de lma valeur absolue
+* 
+* retour: à r 
+*/
+int _abs(int r)
+{
+	if (r < 0)
+		return (-r);
+	else
+		return (r);
+}
